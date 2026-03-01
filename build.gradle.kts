0a15323a9ebf7e51ec907490b9c6ad17ed04f415
mk7a <git@mk7.dev>
2026-03-01 18:43:54 +0100

    Migrate build to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    java
+    id("com.gradleup.shadow") version "9.0.0-beta12"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "com.mk7a"
+version = "1.8.2"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.codemc.org/repository/maven-public")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.8-R0.1-SNAPSHOT")
+    implementation("org.bstats:bstats-bukkit:3.1.0")
+}
+
+tasks.shadowJar {
+    relocate("org.bstats", "com.mk7a.soulbind.bstats")
+    minimize()
+    archiveClassifier.set("")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.processResources {
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.runServer {
+    minecraftVersion("1.21.11")
+}
